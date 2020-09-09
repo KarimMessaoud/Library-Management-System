e9c61875d32432f28142a535ed4911fc1b70374e
Karim Messaoud <karim7@wp.pl>
2020-09-09 15:46:37 +0200

    Created HomeController

--- Library/Controllers/HomeController.cs
+++ Library/Controllers/HomeController.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace Library.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
-
     }
 }
