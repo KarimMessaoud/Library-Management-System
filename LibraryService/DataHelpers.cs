75759553dc51dee006a4b24c90cce25825aabd63
Karim Messaoud <karim7@wp.pl>
2020-09-09 14:39:32 +0200

    Created branch service

---
+++ LibraryService/DataHelpers.cs
@@ -0,0 +1,36 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryService
+{
+    public class DataHelpers
+    {
+        public static List<string> HumanizeBizHours(IEnumerable<BranchHours> branchHours)
+        {
+            var hours = new List<string>();
+
+            foreach (var item in branchHours)
+            {
+                var day = HumanizeDay(item.DayOfWeek);
+                var openTime = HumanizeTime(item.OpenTime);
+                var closeTime = HumanizeTime(item.CloseTime);
+
+                var timeEntry = $"{day} {openTime} to {closeTime}";
+                hours.Add(timeEntry);
+            }
+
+            return hours;
+        }
+
+        private static string HumanizeTime(int time)
+        {
+            return TimeSpan.FromHours(time).ToString("hh':'mm");
+        }
+
+        private static string HumanizeDay(int number)
+        {
+            return Enum.GetName(typeof(DayOfWeek), number - 1);
+        }
+    }
+}
---
+++ LibraryService/LibraryBranchService.cs
@@ -0,0 +1,81 @@
+﻿using LibraryData;
+using LibraryData.Models;
+using LibraryData.Models.Account;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryService
+{
+    public class LibraryBranchService : ILibraryBranch
+    {
+        private LibraryContext _context;
+        public LibraryBranchService(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(LibraryBranch newBranch)
+        {
+            _context.Add(newBranch);
+            _context.SaveChanges();
+        }
+
+        public LibraryBranch GetBranchById(int branchId)
+        {
+            return GetAll()
+                .FirstOrDefault(x => x.Id == branchId);
+        }
+
+        public LibraryBranch GetBranchByName(string branchName)
+        {
+            return GetAll()
+                .FirstOrDefault(x => x.Name == branchName);
+        }
+
+        public IEnumerable<LibraryBranch> GetAll()
+        {
+            return _context.LibraryBranches
+                .Include(x => x.Patrons)
+                .Include(x => x.LibraryAssets);
+        }
+
+        public IEnumerable<LibraryAsset> GetAssets(int branchId)
+        {
+            return _context.LibraryBranches
+                .Include(x => x.LibraryAssets)
+                .FirstOrDefault(x => x.Id == branchId)
+                .LibraryAssets;
+        }
+
+        public IEnumerable<string> GetBranchHours(int branchId)
+        {
+            var hours = _context.BranchHours
+                .Where(x => x.Branch.Id == branchId);
+
+            return DataHelpers.HumanizeBizHours(hours);
+        }
+
+        public IEnumerable<User> GetPatrons(int branchId)
+        {
+            return _context.LibraryBranches
+                .Include(x => x.Patrons)
+                .FirstOrDefault(x => x.Id == branchId)
+                .Patrons;
+        }
+
+        public bool IsBranchOpen(int branchId)
+        {
+            var currentTimeHour = DateTime.Now.Hour;
+            var currentDayOfWeek = (int)DateTime.Now.DayOfWeek;
+
+            var hours = _context.BranchHours
+                .Where(x => x.Branch.Id == branchId);
+
+            var daysHours = hours.FirstOrDefault(x => x.DayOfWeek == currentDayOfWeek);
+
+            return currentTimeHour < daysHours.OpenTime && currentTimeHour > daysHours.CloseTime;
+        }
+    }
+}
