5b29f60cfd35bfb1ee5532ea58ad52e773f7a9e8
Karim Messaoud <karim7@wp.pl>
2020-09-11 19:55:49 +0200

    Added Edit action to PatronController

--- Library/Controllers/PatronController.cs
+++ Library/Controllers/PatronController.cs
@@ -1,6 +1,8 @@
 ﻿using Library.Models.Patron;
 using LibraryData;
 using LibraryData.Models;
+using LibraryData.Models.Account;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,9 +14,15 @@ namespace Library.Controllers
     public class PatronController : Controller
     {
         private readonly IPatron _patron;
-        public PatronController(IPatron patron)
+        private readonly ILibraryBranch _branch;
+        private readonly UserManager<User> _userManager;
+        public PatronController(IPatron patron,
+            ILibraryBranch branch,
+            UserManager<User> userManager)
         {
             _patron = patron;
+            _branch = branch;
+            _userManager = userManager;
         }
 
         public IActionResult Index(string searchString)
@@ -78,6 +86,65 @@ namespace Library.Controllers
                 CheckoutHistory = await _patron.GetCheckoutHistory(id),
                 Holds = await _patron.GetHolds(id)
             };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return View("NoIdFound");
+            }
+
+            var patron = _patron.Get(id);
+
+            if (patron == null)
+            {
+                Response.StatusCode = 404;
+                return View("PatronNotFound", id);
+            }
+
+            var model = new PatronEditViewModel
+            {
+                Id = patron.Id,
+                FirstName = patron.FirstName,
+                LastName = patron.LastName,
+                Address = patron.Address,
+                DateOfBirth = patron.DateOfBirth,
+                HomeLibraryBranchName = patron.HomeLibraryBranch.Name,
+                Telephone = patron.PhoneNumber
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(PatronEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var patron = _patron.Get(model.Id);
+
+                if (patron == null)
+                {
+                    Response.StatusCode = 404;
+                    return View("PatronNotFound", model.Id);
+                }
+
+                patron.FirstName = model.FirstName;
+                patron.LastName = model.LastName;
+                patron.Address = model.Address;
+                patron.DateOfBirth = model.DateOfBirth;
+                patron.PhoneNumber = model.Telephone;
+                patron.HomeLibraryBranch = _branch.GetBranchByName(model.HomeLibraryBranchName);
+
+                await _userManager.UpdateAsync(patron);
+
+                return RedirectToAction("Index", "Patron", new { id = patron.Id });
+            }
 
             return View(model);
         }
---
+++ Library/Models/Patron/PatronEditViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models.Patron
+{
+    public class PatronEditViewModel
+    { 
+        public string Id { get; set; }
+
+        [Required] 
+        [MinLength(2)] 
+        [MaxLength(50)]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required, MinLength(2), MaxLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required] 
+        [DataType(DataType.Date)]
+        [Display(Name = "Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [Display(Name = "Telephone")]
+        public string Telephone { get; set; }
+
+        [Required]
+        [Display(Name ="Home Library Branch Name")]
+        public string HomeLibraryBranchName { get; set; }
+    }
+}
