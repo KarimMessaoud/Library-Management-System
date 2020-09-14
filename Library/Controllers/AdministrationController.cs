ba0994b43a662a84e9f18b3731139194dcb9d0c6
Karim Messaoud <karim7@wp.pl>
2020-09-14 21:01:56 +0200

    Created AdministrationController and added RolesList action

---
+++ Library/Controllers/AdministrationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Library.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        
+        [HttpGet]
+        public IActionResult RolesList()
+        {
+            var roles = _roleManager.Roles;
+
+            return View(roles);
+        }
+    }
+}
