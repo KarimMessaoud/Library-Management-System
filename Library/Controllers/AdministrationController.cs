13c4282b1919bd59455a974170d1743a1bd65f4f
Karim Messaoud <karim7@wp.pl>
2020-09-15 17:17:27 +0200

    Added CreateRole action to AdministrationController

--- Library/Controllers/AdministrationController.cs
+++ Library/Controllers/AdministrationController.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Library.Models.Administration;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Threading.Tasks;
 
 namespace Library.Controllers
 {
@@ -21,6 +22,38 @@ namespace Library.Controllers
             var roles = _roleManager.Roles;
 
             return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+
+                var result = await _roleManager.CreateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("RolesList", "Administration");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
         }
     }
 }
---
+++ Library/Models/Administration/CreateRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models.Administration
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [MinLength(2)]
+        [Display(Name="Role name")]
+        public string RoleName { get; set; }
+    }
+}
