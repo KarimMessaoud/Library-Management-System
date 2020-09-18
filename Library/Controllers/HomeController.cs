8b650a91a23f08b5efa2ba58d71c0aa29121d9d4
Karim Messaoud <karim7@wp.pl>
2020-09-18 10:55:40 +0200

    Added authorization to Home page

--- Library/Controllers/HomeController.cs
+++ Library/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ namespace Library.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
