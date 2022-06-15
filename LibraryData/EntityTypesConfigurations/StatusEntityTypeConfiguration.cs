1e7c15de0ae00dfcb98674245c8ac2be3e937cc3
Karim Messaoud <karim7@wp.pl>
2022-06-15 12:02:46 +0200

    Configured Status entity with Fluent API

---
+++ LibraryData/EntityTypesConfigurations/StatusEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.EntityTypesConfigurations
+{
+    public class StatusEntityTypeConfiguration : IEntityTypeConfiguration<Status>
+    {
+        public void Configure(EntityTypeBuilder<Status> builder)
+        {
+            builder.Property(x => x.Name)
+                .HasMaxLength(30)
+                .IsRequired();
+
+            builder.Property(x => x.Description)
+              .HasMaxLength(100)
+              .IsRequired();
+        }
+    }
+}
---
+++ LibraryData/Migrations/20220615095940_Configure Status entity with Fluent API.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryData.Migrations
+{
+    public partial class ConfigureStatusentitywithFluentAPI : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Statuses",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Statuses",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Statuses",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Statuses",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+    }
+}
