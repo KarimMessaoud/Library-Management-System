cdf145e11f9280438e7bf81cad3a1bef554851ef
Karim Messaoud <karim7@wp.pl>
2022-06-15 12:51:15 +0200

    Configured User entity with Fluent API - continuation

--- LibraryData/EntityTypesConfigurations/UserEntityTypeConfiguration.cs
+++ LibraryData/EntityTypesConfigurations/UserEntityTypeConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using LibraryData.Models.Account;
+﻿using LibraryData.Models;
+using LibraryData.Models.Account;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -9,52 +10,54 @@ namespace LibraryData.EntityTypesConfigurations
     {
         public void Configure(EntityTypeBuilder<User> builder)
             {
-                builder.HasOne(x => x.LibraryCard);
+                builder.HasOne(x => x.LibraryCard)
+                    .WithOne(x => x.Patron)
+                    .HasForeignKey<LibraryCard>(x => x.PatronId);
 
                 builder.HasOne(x => x.HomeLibraryBranch)
-                       .WithMany(x => x.Patrons)
-                       .HasForeignKey("HomeLibraryBranchId");
+                    .WithMany(x => x.Patrons)
+                    .HasForeignKey("HomeLibraryBranchId");
 
                 builder.Property(x => x.FirstName)
                    .HasMaxLength(50)
                    .IsRequired();
 
                 builder.Property(x => x.LastName)
-                       .HasMaxLength(50)
-                       .IsRequired();
+                   .HasMaxLength(50)
+                   .IsRequired();
 
                 builder.Property(x => x.Address)
                    .HasMaxLength(100)
                    .IsRequired();
 
                 builder.Property(x => x.Email)
-                       .HasMaxLength(256)
-                       .IsRequired();
+                    .HasMaxLength(256)
+                    .IsRequired();
 
                 builder.Property(x => x.DateOfBirth)
-                       .HasColumnType("date")
-                       .IsRequired();
+                    .HasColumnType("date")
+                    .IsRequired();
                 
 
                 //The below configuration is based on the following article: https://joshthecoder.com/2020/04/28/max-data-types-in-aspnet-core-identity-schema.html
                 //and: https://github.com/dotnet/aspnetcore/issues/5823
                 builder.Property(x => x.PhoneNumber).HasMaxLength(50)
-                                                    .IsUnicode(false)
-                                                    .IsFixedLength(false);
+                    .IsUnicode(false)
+                    .IsFixedLength(false);
 
                 builder.Property(x => x.PasswordHash).HasMaxLength(84)
-                                                     .IsUnicode(false)
-                                                     .IsFixedLength(true);
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
 
                 builder.Property(x => x.SecurityStamp).HasMaxLength(36)
-                                                      .IsUnicode(false)
-                                                      .IsFixedLength(false)
-                                                      .IsRequired(true);
+                    .IsUnicode(false)
+                    .IsFixedLength(false)
+                    .IsRequired(true);
 
                 builder.Property(x => x.ConcurrencyStamp).HasMaxLength(36)
-                                                         .IsUnicode(false)
-                                                         .IsFixedLength(true)
-                                                         .IsRequired(true);
+                    .IsUnicode(false)
+                    .IsFixedLength(true)
+                    .IsRequired(true);
             }
     }
 }
---
+++ LibraryData/Migrations/20220615103252_Configure User entity with Fluent API - continuation.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryData.Migrations
+{
+    public partial class ConfigureUserentitywithFluentAPIcontinuation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_LibraryCards_LibraryCardId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_LibraryCardId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LibraryCardId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PatronId",
+                table: "LibraryCards",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibraryCards_PatronId",
+                table: "LibraryCards",
+                column: "PatronId",
+                unique: true,
+                filter: "[PatronId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LibraryCards_AspNetUsers_PatronId",
+                table: "LibraryCards",
+                column: "PatronId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LibraryCards_AspNetUsers_PatronId",
+                table: "LibraryCards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LibraryCards_PatronId",
+                table: "LibraryCards");
+
+            migrationBuilder.DropColumn(
+                name: "PatronId",
+                table: "LibraryCards");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LibraryCardId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_LibraryCardId",
+                table: "AspNetUsers",
+                column: "LibraryCardId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_LibraryCards_LibraryCardId",
+                table: "AspNetUsers",
+                column: "LibraryCardId",
+                principalTable: "LibraryCards",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
